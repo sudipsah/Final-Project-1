f8e91e3337cda412d8cd7a96d73b64601b7c2c74
sudipshah <sudipshah@192.168.1.101>
2018-05-28 20:25:51 +0600

    upadated1

--- Spl/src/SPL/Data.java
+++ Spl/src/SPL/Data.java
@@ -9,8 +9,8 @@ public class Data {
 
 
 	public static void main(String args[]) throws IOException{
-		BufferedReader bufReader = new BufferedReader(new FileReader("/Users/sudipshah/git/SPL-1/Spl/src/SPL/file.txt"));
-		String isbn1,isbn2,isbn3;
+		BufferedReader bufReader = new BufferedReader(new FileReader("/Users/sudipshah/git/SPL-1/Spl/file.txt"));
+		String isbn1,isbn2,isbn3,isbn4,isbn5;
 		ArrayList<String> listOfLines = new ArrayList<>(); 
 		String  line = bufReader.readLine();
 		while (line != null) { 	
@@ -19,12 +19,20 @@ public class Data {
 		isbn1=listOfLines.get(0);
 		isbn2=listOfLines.get(1);
 		isbn3=listOfLines.get(2);
+		isbn4=listOfLines.get(3);
+		isbn5=listOfLines.get(4);
+
 		ISBN1 value =new ISBN1();
-		value.Data1(isbn1);
+		//value.Data1(isbn1);
 		ISBN2 value1 = new ISBN2();
-		value1.Data1(isbn2);
+	//	value1.Data1(isbn2);
 		ISBN3 value2 = new ISBN3();
-		value2.Data1(isbn3);
+		//value2.Data1(isbn3);
+		ISBN4 value3 = new ISBN4();
+		value3.Data1(isbn4);
+		ISBN5 value4 = new ISBN5();
+		value4.Data1(isbn5);
+
 
 
 		/*GUI method1 = new GUI();
