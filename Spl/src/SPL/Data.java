05a807311dc5c1713ecb84a386839f2fe26a0616
sudipshah <sudipshah@192.168.1.101>
2018-05-29 01:03:24 +0600

    lastupdate

--- Spl/src/SPL/Data.java
+++ Spl/src/SPL/Data.java
@@ -10,7 +10,7 @@ public class Data {
 
 	public static void main(String args[]) throws IOException{
 		BufferedReader bufReader = new BufferedReader(new FileReader("/Users/sudipshah/git/SPL-1/Spl/file.txt"));
-		String isbn1,isbn2,isbn3,isbn4,isbn5;
+		String isbn1,isbn2,isbn3,isbn4;
 		ArrayList<String> listOfLines = new ArrayList<>(); 
 		String  line = bufReader.readLine();
 		while (line != null) { 	
@@ -20,26 +20,17 @@ public class Data {
 		isbn2=listOfLines.get(1);
 		isbn3=listOfLines.get(2);
 		isbn4=listOfLines.get(3);
-		isbn5=listOfLines.get(4);
+		
 
 		ISBN1 value =new ISBN1();
-		//value.Data1(isbn1);
+		value.Data1(isbn1);
 		ISBN2 value1 = new ISBN2();
-	//	value1.Data1(isbn2);
+		value1.Data1(isbn2);
 		ISBN3 value2 = new ISBN3();
-		//value2.Data1(isbn3);
+		value2.Data1(isbn3);
 		ISBN4 value3 = new ISBN4();
 		value3.Data1(isbn4);
-		ISBN5 value4 = new ISBN5();
-		value4.Data1(isbn5);
-
-
-
-		/*GUI method1 = new GUI();
-		method1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		method1.setSize(500,200);
-		method1.setVisible(true);
-		method1.setTitle("Book's Infromation");*/
+	
 
 
 
